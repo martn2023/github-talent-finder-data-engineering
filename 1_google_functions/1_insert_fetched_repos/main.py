1cbe608863f51f3a9f8f4760fefe5b1b2d652e7a
Martin <42924628+martn2022@users.noreply.github.com>
2024-10-17 03:28:10 -0400

    connection proven

--- 1_google_functions/1_insert_fetched_repos/main.py
+++ 1_google_functions/1_insert_fetched_repos/main.py
@@ -27,6 +27,9 @@ def get_db_credentials_from_secret_manager(version_id="latest"):
 
 
 def call_github_search(pat: str):
+    url = "https://api.github.com/search/repositories?q=pushed%3A2024-09-02T00%3A00%3A00..2024-09-02T00%3A01%3A00+is%3Apublic+-fork%3Atrue&per_page=100&page=1"
+    print(f"PRINTING default url: {url}")
+
     db_credentials_not_json = get_db_credentials_from_secret_manager()
     db_credentials = json.loads(db_credentials_not_json)
     print("PRINTING db_credentials")
@@ -34,44 +37,51 @@ def call_github_search(pat: str):
 
     # intentionally leaving potentially duplicative code in here until I prove the automation works
 
-    connection = psycopg2.connect(
-        host=db_credentials['DB_HOST'],
-        port=db_credentials['DB_PORT'],
-        database=db_credentials['DB_NAME'],
-        user=db_credentials['DB_USER'],
-        password=db_credentials['DB_PASS']
-    )
+    try:
+        connection = psycopg2.connect(
+            host=db_credentials['DB_HOST'],
+            port=db_credentials['DB_PORT'],
+            database=db_credentials['DB_NAME'],
+            user=db_credentials['DB_USER'],
+            password=db_credentials['DB_PASS']
+        )
 
-    print("PRINTING connection made")
-    # print(connection)
-    # connection.close()
-    print("PRINTING connection closed")
+        connection.close()
+        print("PRINTING connection closed")
 
-    # cursor = connection.cursor()
-    # cursor.execute("SELECT last_call FROM task_scheduling WHERE function_name = %s;", ("scheduled_get_repos",))
+    except:
+        print("PRINTING connection NOT made")
 
-    # this is the part that will take the call time out of the 3rd table
-    # last_call_time = cursor.fetchone()[0]
+    '''
 
-    # cursor.close()
+    cursor = connection.cursor()
+    cursor.execute("SELECT last_call FROM task_scheduling WHERE function_name = %s;", ("scheduled_get_repos",))
+
+
+    this is the part that will take the call time out of the 3rd table
+    last_call_time = cursor.fetchone()[0]
+
+    cursor.close()
 
     print("PRINTING connection and cursor closed")
 
-    last_call_time_string = "2024-10-13T00:00:00Z"
+
+    #last_call_time_string = "2024-10-13T00:00:00Z"
     last_call_time = datetime.strptime(last_call_time_string, "%Y-%m-%dT%H:%M:%SZ")
 
-    # we are only adding 1 minute at a time to get ~70 results per search. If we had done a full hour, it would be 5000 hits, well over the 1000 result limit set by GitHub API
+    #we are only adding 1 minute at a time to get ~70 results per search. If we had done a full hour, it would be 5000 hits, well over the 1000 result limit set by GitHub API
     next_call_time = last_call_time + timedelta(minutes=1)
 
     # reformatting for API URL
     start_time = last_call_time.strftime("%Y-%m-%dT%H:%M:%S")
     end_time = next_call_time.strftime("%Y-%m-%dT%H:%M:%S")
 
-    url = f"https://api.github.com/search/repositories?q=pushed%3A{start_time}..{end_time}+is%3Apublic+-fork%3Atrue&per_page=100&page=1"
-    print("proposed url")
-    print(url)
+    '''
 
-    # url = "https://api.github.com/search/repositories?q=pushed%3A2024-09-01T00%3A00%3A00..2024-09-01T00%3A01%3A00+is%3Apublic+-fork%3Atrue&per_page=100&page=1"
+    try:
+        url = f"https://api.github.com/search/repositories?q=pushed%3A{start_time}..{end_time}+is%3Apublic+-fork%3Atrue&per_page=100&page=1"
+    except:
+        print("PRINTING exception, url unchanged")
 
     headers = {
         "Authorization": f"Bearer {pat}",
@@ -81,37 +91,6 @@ def call_github_search(pat: str):
                                         headers=headers)  # WARNING, this is coming in as an HTTP response object, not a JSON just yet
     github_search_results_in_json = http_response_object.json()
     return github_search_results_in_json
-
-
-# this code is likely obsolete now
-def select_all_from_db(db_credentials):
-    exposed_host = db_credentials['DB_HOST']
-    exposed_port = db_credentials['DB_PORT']
-    exposed_db_name = db_credentials['DB_NAME']
-    exposed_user = db_credentials['DB_USER']
-    exposed_password = db_credentials['DB_PASS']
-
-    # WARNING, THE BELOW ARE RESERVED TERMS
-    try:
-        attempted_db_connection = psycopg2.connect(
-            host=exposed_host,
-            port=exposed_port,
-            database=exposed_db_name,  # term of art, can't use NAME
-            user=exposed_user,
-            password=exposed_password
-        )
-
-        cursor = attempted_db_connection.cursor()  # need explanation here
-        cursor.execute("SELECT * FROM github_repos;")  # puts the data in the cursor, but still need to get it out
-        rows_of_data = cursor.fetchall()  # we cant rely on cursor anymore, because it demands open connection
-
-        cursor.close()  # ask why this is imperative
-        attempted_db_connection.close()  # ask why this is imperative
-
-        return rows_of_data  # this is a list of tuples
-
-    except Exception as reported_error:
-        return f"Connecting to database, due to {reported_error}"
 
 
 def get_github_repos(request):
@@ -197,3 +176,35 @@ def get_github_repos(request):
 
     # full_table_data = select_all_from_db(db_credentials)
     # return full_table_data
+
+
+''' this code is likely obsolete now but keep it for debugging purposes
+def select_all_from_db(db_credentials):
+    exposed_host = db_credentials['DB_HOST']
+    exposed_port = db_credentials['DB_PORT']
+    exposed_db_name = db_credentials['DB_NAME']
+    exposed_user = db_credentials['DB_USER']
+    exposed_password = db_credentials['DB_PASS']
+
+    # WARNING, THE BELOW ARE RESERVED TERMS
+    try:
+        attempted_db_connection = psycopg2.connect(
+            host = exposed_host,
+            port = exposed_port,
+            database = exposed_db_name, # term of art, can't use NAME
+            user = exposed_user,
+            password = exposed_password
+        )
+
+        cursor = attempted_db_connection.cursor() # need explanation here
+        cursor.execute("SELECT * FROM github_repos;") # puts the data in the cursor, but still need to get it out
+        rows_of_data = cursor.fetchall() #we cant rely on cursor anymore, because it demands open connection
+
+        cursor.close() # ask why this is imperative
+        attempted_db_connection.close() # ask why this is imperative
+
+        return rows_of_data # this is a list of tuples
+
+    except Exception as reported_error:
+        return f"Connecting to database, due to {reported_error}"
+'''
